ef04ff75c2fc51ca2ff0605f4c68f74f838ad82d
Mateusz Wiśniewski <siwypl@o2.pl>
2018-04-16 00:18:57 +0200

    Base Model and DbContext with config

---
+++ PizzaWebAPI/PizzaWebAPI/Model/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaWebAPI.Model
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string EmailVerificationHash { get; set; }
+        public bool IsConfirmed { get; set; }
+        public bool IsActive { get; set; }
+        public List<AccountRole> Roles { get; set; }
+    }
+}
---
+++ PizzaWebAPI/PizzaWebAPI/Model/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaWebAPI.Model
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options) {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountRole> AccountsRoles { get; set; }
+    }
+}
